8ba5775ae0453418c65604994e378bb2b52bbc87
fahimehzarei <fahimeh.zarei412@gmail.com>
2025-09-30 22:10:12 +0330

    Add files via upload

---
+++ logfile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+logfile="/var/log/auth.log"
+senderEmail="daftar.home@gmail.com"
+nowDate=$(date '+%Y-%m-%d %H:%M:%S')
+
+# check logfile to exist
+if [ ! -f "$logfile" ]; then
+    echo "$nowDate : log file not found"
+    exit 1
+fi
+
+# Unsuccessful log
+failcount=$(sudo grep "Failed password" $logfile | wc -l)
+
+# Send email if there was an unsuccessful attempt
+if [ $failcount -gt 0 ]; then
+    echo "$nowDate : $failcount failed login attempts" | mail -s "Failed Login Alert" $senderEmail
+else
+    echo "$nowDate : no failed login attempts"
+fi
